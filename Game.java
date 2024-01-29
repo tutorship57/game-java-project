953a258edd33e78d54d0ed9f659ff2945683b065
Phongkiatr <phongkiat2548@gmail.com>
2024-01-29 20:59:51 +0700

    GAY

--- Game.java
+++ Game.java
@@ -5,15 +5,23 @@ public class Game {
 
     public Game() {
         JFrame frame = new JFrame("My Tutor Game");
-        ImageIcon icon = new ImageIcon("src/icon.jpg");
-
+        
         frame.setTitle("My Tutor Game");
-        frame.setSize(400, 300);
+        frame.setBounds(50, 50, 200, 30);
+        frame.setSize(1080, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // set Icon
+        ImageIcon icon = new ImageIcon("src/icon.jpg");
         frame.setIconImage(icon.getImage());
+
+        // set Background
+        ImageIcon background = new ImageIcon("src/background.jpg");
+        JLabel backgroundLabel = new JLabel(background);
+        backgroundLabel.setLayout(new BorderLayout());
+        backgroundLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        frame.add(backgroundLabel);
         frame.setVisible(true);
-    }
-    public static void main(String[] args) {
-        Game tutor = new Game();
     }
 }
