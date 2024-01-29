5c5c730745d0f3b98b3504efac5be69d20df09d7
Phongkiatr <phongkiat2548@gmail.com>
2024-01-29 21:11:42 +0700

    maximum size

--- Game.java
+++ Game.java
@@ -4,11 +4,12 @@ import java.awt.*;
 public class Game {
 
     public Game() {
-        JFrame frame = new JFrame("My Tutor Game");
+        JFrame frame = new JFrame();
         
         frame.setTitle("My Tutor Game");
         frame.setBounds(50, 50, 200, 30);
         frame.setSize(1080, 720);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // set Icon
@@ -18,8 +19,8 @@ public class Game {
         // set Background
         ImageIcon background = new ImageIcon("src/background.jpg");
         JLabel backgroundLabel = new JLabel(background);
-        backgroundLabel.setLayout(new BorderLayout());
         backgroundLabel.setHorizontalAlignment(JLabel.CENTER);
+        backgroundLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
 
         frame.add(backgroundLabel);
         frame.setVisible(true);
