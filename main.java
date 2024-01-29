416d43192b97d8443ac94e1b4b98893cc8acf324
worldwhilehandsomeface <gamertvgrean@gmail.com>
2024-01-29 19:39:49 +0700

    oom

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ class main {
     public static void main(String[] args) {
         System.out.println("kuy");
         System.out.println("nahee game sunny ");
+        System.out.println("ho ho");
     }
 }
